ddd36a89d807bba6749ff4e13016272050175ca3
krylchuk <krultchuk@gmail.com>
2024-10-16 22:05:21 +0200

    changed route

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,16 +4,13 @@ import '../style/main.scss';
 import Header from '../components/header';
 import Footer from '../components/footer';
 
-
-
-
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
+  src: "../fonts/GeistVF.woff",
   variable: "--font-geist-sans",
   weight: "100 900",
 });
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
+  src: "../fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
   weight: "100 900",
 });
